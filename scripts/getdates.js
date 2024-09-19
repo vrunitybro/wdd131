94b559c072550ea9d4b11bb86097e390b38886ee
davidcarrillobyui <vrunitybro@gmail.com>
2024-09-18 23:45:36 -0600

    js added

---
+++ scripts/getdates.js
@@ -0,0 +1,7 @@
+// Example JavaScript code that you can modify as needed
+
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('JavaScript is working!');
+    // You can add more functionality here as needed
+  });
+  
