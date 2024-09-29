61ada818e94c0e8e7c5ada5260b31b0dab808682
davidcarrillobyui <vrunitybro@gmail.com>
2024-09-29 01:35:24 -0600

    grid large fixed

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,9 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
     const hamburger = document.querySelector('.hamburger');
-    const nav = document.querySelector('nav');
+    const nav = document.querySelector('.navbar');
 
     hamburger.addEventListener('click', function() {
         nav.classList.toggle('active');
-        hamburger.classList.toggle('active');
     });
 });
